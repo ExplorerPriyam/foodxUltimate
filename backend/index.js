c2e66401657315d8e9439a487d10e0d1036235d0
Priyam Chakraborty <priyam4201@gmail.com>
2023-12-04 21:30:13 +0530

    local host added in cors

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
 const express = require('express')
-const cors =require("cors");
+const cors = require("cors");
 const app = express()
 const port = 5000
 const mongoDB = require("./db")
-app.use(cors());
-app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
-  res.header("Access-Control-Allow-Headers","Origin, X-Requested-With,Content-Type, Accept");
+app.use(cors(
+  {
+    origin: "http://localhost:3000"
+  }
+));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,Content-Type, Accept");
   next();
 })
 mongoDB();
@@ -14,9 +18,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.use(express.json());
-app.use("/api",require("./routes/CreateUser"));
-app.use("/api",require("./routes/DisplayData"));
-app.use("/api",require("./routes/OrderData"));
+app.use("/api", require("./routes/CreateUser"));
+app.use("/api", require("./routes/DisplayData"));
+app.use("/api", require("./routes/OrderData"));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
