4626dcaa1f04901ea53d0aaaf777bf01c6edbed0
Priyam Chakraborty <priyam4201@gmail.com>
2023-12-06 23:42:03 +0530

    local to vercel

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(cors(
   }
 ));
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://foodx-ultimate-priyam-chakrabortys-projects.vercel.app:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,Content-Type, Accept");
   next();
 })
